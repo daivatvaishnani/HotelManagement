d0bb6ac9f42824ffbe98f159d65442c37dad0255
Yagyansh <yagyanshskumar@gmail.com>
2017-11-18 18:35:10 +0530

    Guest Activity New Proceedings.

--- app/src/main/java/com/example/shivam/HotelManagement/Activity/GuestActivity.java
+++ app/src/main/java/com/example/shivam/HotelManagement/Activity/GuestActivity.java
@@ -230,17 +230,15 @@ public class GuestActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_roomservice) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_feedback) {
 
         }
 
